b6efcb606508b61afa8b2f0c2ae67aaae866b887
Dawson Sallee <dawsonsallee13@gmail.com>
2025-10-09 09:16:42 -0400

    FINISHED DieselTruck

--- Week 8/Assignment_5_Inheritance/src/DieselTruck.java
+++ Week 8/Assignment_5_Inheritance/src/DieselTruck.java
@@ -1,3 +1,48 @@
-public class DieselTruck {
+public class DieselTruck extends Vehicle implements FuelPowered {
     
+    private double fuelGallons = 0;
+
+    public DieselTruck(String vehicleID, int maxSpeed, double intfuelGallons) {
+        
+        super(vehicleID, maxSpeed);
+        this.fuelGallons = intfuelGallons;
+    }
+
+    @Override
+    public double checkFuelLevel() {
+        return this.fuelGallons;
+    }
+
+    @Override
+    public boolean needsService() {
+        return checkFuelLevel() < 10;
+    }
+
+    @Override
+    public String getPowerStatus() {
+
+        String status = String.format("Fuel: %.2f gal", checkFuelLevel());
+
+        if(needsService()) {
+            status += " (CRITICAL)";
+        }
+
+        return status;
+    }
+    
+    @Override
+    public void refuel() {
+        this.fuelGallons = 50;
+    }
+
+    @Override
+    public String performMaintenance() {
+        refuel();
+        return "Fluid levels good. Refueling truck.";
+    }
+
+    @Override
+    public String getEngineType() {
+        return "Diesel";
+    }
 }
